e0bff8c7c659916ffaf0aa825461b9d9fbfe7401
Jean <jeanmattes@gmx.de>
2021-08-30 16:28:15 +0200

    updated docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+git pull
 sudo docker-compose rm -s -f dvut-s
 sudo docker-compose up -d --remove-orphans --no-deps --build dvut-s
+sudo docker ps -a
+curl 172.20.0.5
+sudo docker-compose logs
